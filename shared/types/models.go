39cd4e2530923aaea101fff09af294e9eb1cc243
Abdo Hafez <abdohafez731@gmail.com>
2026-05-07 15:47:12 +0300

    feat: added shared request and response models

---
+++ shared/types/models.go
@@ -0,0 +1,29 @@
+package types
+
+type Response struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
+type CreateDatabaseRequest struct {
+	Database string `json:"database"`
+}
+
+type CreateTableRequest struct {
+	Database string            `json:"database"`
+	Table    string            `json:"table"`
+	Columns  map[string]string `json:"columns"`
+}
+
+type InsertRequest struct {
+	Database string                 `json:"database"`
+	Table    string                 `json:"table"`
+	Data     map[string]interface{} `json:"data"`
+}
+
+type ReplicationRequest struct {
+	Operation string                 `json:"operation"`
+	Database  string                 `json:"database"`
+	Table     string                 `json:"table"`
+	Data      map[string]interface{} `json:"data"`
+}
